0e4a1df89222bcb45fdd6648c9c1ae87fb1429f4
JariHuomo <jari.huomo@gmail.com>
2025-02-12 21:49:44 +0200

    Added download button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -406,25 +406,50 @@ export default function ImagenTest() {
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
                 
-                {/* Delete Button */}
-                <button
-                  onClick={(e) => handleDeleteImage(image, e)}
-                  className="absolute top-2 right-2 p-2 rounded-full bg-red-500/80 hover:bg-red-600/80 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
-                  aria-label="Delete image"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
+                {/* Action Buttons */}
+                <div className="absolute top-2 right-2 flex gap-2">
+                  {/* Download Button */}
+                  <a
+                    href={image.url}
+                    download={`imagen-${image.id}.png`}
+                    onClick={(e) => e.stopPropagation()}
+                    className="p-2 rounded-full bg-blue-500/80 hover:bg-blue-600/80 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
+                    aria-label="Download image"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                  
+                  {/* Delete Button */}
+                  <button
+                    onClick={(e) => handleDeleteImage(image, e)}
+                    className="p-2 rounded-full bg-red-500/80 hover:bg-red-600/80 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"
+                    aria-label="Delete image"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
               <div className="absolute bottom-0 left-0 right-0 p-4 transform translate-y-2 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300">
                 <p className="text-sm text-white/90 line-clamp-2 mb-2">{image.prompt}</p>
